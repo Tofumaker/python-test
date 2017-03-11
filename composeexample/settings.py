ac0c42ed9e0f553313075282c11251dd9a9fa33d
Tofumaker <dyy2105@columbia.edu>
2017-03-11 13:27:16 -0500

    change back

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'composeexample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'HOST': 'db',
